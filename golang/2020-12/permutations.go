aa785c5dd6be4cda128d4929a8717723af8ccb12
xdd <80866702@qq.com>
2020-12-14 13:56:31 +0800

    :sparkles: Permutations

--- golang/2020-12/permutations.go
+++ golang/2020-12/permutations.go
@@ -3,18 +3,28 @@ package golang
 // Permutations
 // leetCode: https://leetcode-cn.com/problems/permutations/
 
-// TODO 未完成
 func Permute(nums []int) (ans [][]int) {
 	path := make([]int, 0)
+	l := len(nums)
+	used :=  make([]bool, l)
 	var dfs func(index int)
 	dfs = func(index int) {
-		if index == len(nums) {
-			ans = append(ans, path)
+		if index == l {
+			temp := make([]int, l)
+			copy(temp, path)
+			ans = append(ans, temp)
+			return
 		}
-		for i := index; i < len(nums); i++ {
-			path = append(path, nums[i])
-			dfs(i+1)
-			path = path[:len(path)-1]
+		for i := 0; i < l; i++ {
+			if !used[i] {
+				used[i] = true
+				path = append(path, nums[i])
+				//fmt.Println("回溯前==>", path, used, index)
+				dfs(index+1)
+				used[i] = false
+				path = path[:len(path)-1]
+				//fmt.Println("回溯后==>", path, used, index)
+			}
 		}
 	}
 	dfs(0)

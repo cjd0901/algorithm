37f0670de71e9f42164d13ee059cf43832043186
xdd <a80866702@gmail.com>
2021-05-11 17:29:54 +0800

    :sparkles: dp&贪心：摆动排序

--- golang/2020-12/wiggleSubsequence.go
+++ golang/2020-12/wiggleSubsequence.go
@@ -3,7 +3,38 @@ package golang
 // Wiggle Subsequence
 // leetCode: https://leetcode-cn.com/problems/wiggle-subsequence/
 
-func WiggleSubsequence(nums []int) (I int) {
+func WiggleSubsequence(nums []int) int {
+	n := len(nums)
+	if n < 2 {
+		return n
+	}
+	up := make([]int, n)
+	down := make([]int, n)
+	up[0] = 1
+	down[0] = 1
+	for i := 1; i < n; i++ {
+		if nums[i] > nums[i-1] {
+			up[i] = max(up[i-1], down[i-1]+1)
+			down[i] = down[i-1]
+		}else if nums[i] < nums[i-1] {
+			down[i] = max(up[i-1]+1, down[i-1])
+			up[i] = up[i-1]
+		}else {
+			up[i] = up[i-1]
+			down[i] = down[i-1]
+		}
+	}
+	return max(up[n-1], down[n-1])
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func WiggleSubsequence2(nums []int) (I int) {
 	var previous string
 	if len(nums) < 2 {
 		return len(nums)

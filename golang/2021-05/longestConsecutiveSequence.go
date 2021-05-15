1a754632771ea5f4562394f3615049ffb725a91a
xdd <a80866702@gmail.com>
2021-05-16 00:03:25 +0800

    TODO: 最长连续序列

---
+++ golang/2021-05/longestConsecutiveSequence.go
@@ -0,0 +1,50 @@
+package _021_05
+
+import (
+	"sort"
+)
+
+// Longest Consecutive Sequence
+// leetcode: https://leetcode-cn.com/problems/longest-consecutive-sequence/
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+func LongestConsecutiveSequence(nums []int) int {
+	sort.Ints(nums)
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+		dp[i][i] = 1
+	}
+	for i := n-2; i >= 0; i-- {
+		for j := i+1; j < n; j++ {
+			if nums[j] == nums[j-1]+1 {
+				if dp[i+1][j] != dp[i][j-1] {
+					dp[i][j] = min(dp[i+1][j]+1, dp[i][j-1]+1)
+				}else {
+					dp[i][j] = dp[i+1][j]+1
+				}
+			}else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][n-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
---
+++ golang/2021-05/longestConsecutiveSequence_test.go
@@ -0,0 +1,11 @@
+package _021_05
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLongestConsecutiveSequence(t *testing.T) {
+	l := LongestConsecutiveSequence([]int{9,1,4,7,3,-1,0,5,8,-1,6})
+	fmt.Println(l)
+}

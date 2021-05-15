6af3718324204b9927d3d04995b6a6445d5652c9
xdd <80866702@qq.com>
2021-05-15 23:48:16 +0800

    :sparkles: 罗马数字转整数

---
+++ golang/2021-05/romanToInteger.go
@@ -0,0 +1,19 @@
+package _021_05
+
+// Roman to Integer
+// leetcode: https://leetcode-cn.com/problems/roman-to-integer/
+
+func RomanToInteger(s string) int {
+	var symbolValues = map[byte]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+	ans := 0
+	n := len(s)
+	for i := range s {
+		value := symbolValues[s[i]]
+		if i < n-1 && value < symbolValues[s[i+1]] {
+			ans -= value
+		} else {
+			ans += value
+		}
+	}
+	return ans
+}
---
+++ golang/2021-05/romanToInteger_test.go
@@ -0,0 +1,11 @@
+package _021_05
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRomanToInteger(t *testing.T) {
+	ans := RomanToInteger("III")
+	fmt.Println(ans)
+}

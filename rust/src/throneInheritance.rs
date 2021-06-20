bdfd44bb23f75e12e81461d6bf1e1dfe49288193
xdd <80866702@qq.com>
2021-06-20 20:14:20 +0800

    :sparkles: 皇位继承顺序

--- rust/src/throneInheritance.rs
+++ rust/src/throneInheritance.rs
@@ -1,34 +1,49 @@
 // Throne Inheritance
 // leetcode: https://leetcode-cn.com/problems/throne-inheritance/
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 struct ThroneInheritance {
     king: String,
-    edges: HashMap<String, Vec<String>>,
-    death: HashMap<String, bool>,
+    persons: HashMap<String, Vec<String>>,
+    death: HashSet<String>
 }
 
 impl ThroneInheritance {
-
     fn new(kingName: String) -> Self {
-        let edges: HashMap<String, Vec<String>> = HashMap::new();
-        let death: HashMap<String, bool> = HashMap::new();
-        ThroneInheritance{
-            edges,
-            death,
-            king: kingName
-        }
+        let mut ti = ThroneInheritance {
+            persons: HashMap::new(),
+            death: HashSet::new(),
+            king: kingName.clone()
+        };
+        ti.persons.insert(kingName, Vec::new());
+        ti
     }
     
     fn birth(&mut self, parent_name: String, child_name: String) {
-        self.edges.get(&parent_name).unwrap().push(child_name);
-        println!("{:?}", self.edges.get(&parent_name).unwrap());
+        self.persons.insert(child_name.clone(), Vec::new());
+        self.persons.get_mut(&parent_name).unwrap().push(child_name);
     }
     
     fn death(&mut self, name: String) {
-        self.death.insert(name, true);
+        self.death.insert(name);
     }
     
+    fn get_inheritance_order(&self) -> Vec<String> {
+        let mut v = Vec::new();
+        self.preorder(&self.king, &mut v);
+        return v;
+    }
+
+    fn preorder(&self, name: &String, v: &mut Vec<String>) {
+        if !self.death.contains(name) {
+            v.push(name.clone());
+        }
+        if self.persons.contains_key(name) {
+            for child in &self.persons[name] {
+                self.preorder(child, v);
+            }
+        }
+    }
 }
 
 fn main() {

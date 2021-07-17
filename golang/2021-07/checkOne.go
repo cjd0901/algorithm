437ea77a7fcc263cbe5ce8ffb18ea8eae9999ef9
xdd <a80866702@gmail.com>
2021-07-17 23:15:50 +0800

    :sparkles: 是否所有 1 都至少相隔 k 个元素

---
+++ golang/2021-07/checkOne.go
@@ -0,0 +1,18 @@
+package _021_07
+
+// Check If All 1's Are at Least Length K Places Away
+// leetcode: https://leetcode-cn.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+func KLengthApart(nums []int, k int) bool {
+	preIndex := -1
+	for i, n := range nums {
+		if n == 1 {
+			if preIndex == -1 || i - preIndex > k {
+				preIndex = i
+				continue
+			} else {
+				return false
+			}
+		}
+	}
+	return true
+}
---
+++ golang/2021-07/checkOne_test.go
@@ -0,0 +1,11 @@
+package _021_07
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestKLengthApart(t *testing.T) {
+	ans := KLengthApart([]int{0,1,0,1}, 1)
+	fmt.Println(ans)
+}

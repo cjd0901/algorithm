5d4f074c5bee800841823244b137b4f3d4f82949
xdd <80866702@qq.com>
2021-08-15 22:30:37 +0800

    :triumph: 出界的路径数

---
+++ golang/2021-08/outOfBoundaryPaths.go
@@ -0,0 +1,8 @@
+package _021_08
+
+// Out of Boundary Paths
+// leetcode: https://leetcode-cn.com/problems/out-of-boundary-paths/
+func OutOfBoundaryPaths(m int, n int, maxMove int, startRow int, startColumn int) int {
+	mod := int(1e9+7)
+	paths := 0
+}
---
+++ golang/2021-08/outOfBoundaryPaths_test.go
@@ -0,0 +1,11 @@
+package _021_08
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestOutOfBoundaryPaths(t *testing.T) {
+	ans := OutOfBoundaryPaths(8, 7, 16, 1 ,5)
+	fmt.Println(ans)
+}

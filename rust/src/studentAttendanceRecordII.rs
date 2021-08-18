4366bc2f2b87c32636f9c548bc78591884442c15
xdd <a80866702@gmail.com>
2021-08-18 17:58:05 +0800

    :sparkles: 学生的出勤记录II

--- rust/src/studentAttendanceRecordII.rs
+++ rust/src/studentAttendanceRecordII.rs
@@ -8,33 +8,27 @@ impl Solution {
         let mut dp = vec![vec![vec![0; 3]; 2]; (n+1) as usize];
         let mut ans = 0;
         dp[0][0][0] = 1;
-        for i in 1..n+1 {
+        for i in 1..(n+1) as usize {
             // 当当前的记录为'P'时
-            let i = i as usize;
-            for j in 0..2 {
-                let j = j as usize;
-                for k in 0..3 {
-                    let k = k as usize;
+            for j in 0..2 as usize {
+                for k in 0..3 as usize {
                     dp[i][j][0] = (dp[i][j][0] + dp[i-1][j][k]) % MOD;
                 }
             }
             // 当当前的记录为'A'时
-            for k in 0..3 {
-                let k = k as usize;
+            for k in 0..3 as usize {
                 dp[i][1][0] = (dp[i][1][0] + dp[i-1][0][k]) % MOD;
             }
             // 当当前的记录为'L'时
-            for j in 0..2 {
-                let j = j as usize;
-                for k in 1..3 {
-                    let k = k as usize;
+            for j in 0..2 as usize {
+                for k in 1..3 as usize {
                     dp[i][j][k] = (dp[i][j][k] + dp[i-1][j][k-1]) % MOD;
                 }
             }
         }
-        for j in 0..2 {
-            for k in 0..3 {
-                ans = (dp[n as usize][j as usize][k as usize] + ans) % MOD;
+        for j in 0..2 as usize {
+            for k in 0..3 as usize {
+                ans = (dp[n as usize][j][k] + ans) % MOD;
             }
         }
         ans

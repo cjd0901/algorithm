0c706d768bd0afcc53d60e003f3f165c67304d92
xdd <80866702@qq.com>
2021-11-04 15:41:03 +0800

    :sparkles: 颜色分类

--- rust/src-21-10/sortColors.rs
+++ rust/src-21-10/sortColors.rs
@@ -7,28 +7,48 @@ impl Solution {
         let (mut red, mut white) = (0 as usize, 0 as usize);
         for i in 0..nums.len() {
             if nums[i] == 0 {
-                let temp = nums[red];
-                nums[red] = nums[i];
-                nums[i] = temp;
+                swap(nums, i, red);
                 red += 1;
                 if nums[i] == 1 {
-                    let temp = nums[white];
-                    nums[white] = nums[i];
-                    nums[i] = temp;
+                    swap(nums, i, white);
                 }
                 white += 1;
             } else if nums[i] == 1 {
-                let temp = nums[white];
-                nums[white] = nums[i];
-                nums[i] = temp;
+                swap(nums, i, white);
                 white += 1;
+            }
+        }
+    }
+
+    pub fn sort_colors2(nums: &mut Vec<i32>) {
+        let len = nums.len();
+        // [0, p0) == 0
+        // [p0, i) == 1
+        // (p2, len-1] == 2
+        let (mut p0, mut i, mut p2) = (0 as usize, 0 as usize, (len-1) as i32);
+        while i as i32 <= p2 {
+            if nums[i] == 0 {
+                swap(nums, i, p0);
+                p0 += 1;
+                i += 1;
+            } else if nums[i] == 1 {
+                i += 1;
+            } else {
+                swap(nums, i, p2 as usize);
+                p2 -= 1;
             }
         }
     }
 }
 
+fn swap(nums: &mut Vec<i32>, i: usize, j: usize) {
+    let temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
 fn main() {
     let mut nums = vec![2];
-    Solution::sort_colors(&mut nums);
+    Solution::sort_colors2(&mut nums);
     println!("{:?}", nums);
 }

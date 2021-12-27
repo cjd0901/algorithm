dff272f4229b0bf029a0c9614f0428e5dcaae928
xdd <80866702@qq.com>
2021-12-27 17:48:18 +0800

    :sparkles: 组合总和IV

---
+++ golang/2021-12/combinationSumIV.go
@@ -0,0 +1,16 @@
+package _021_12
+
+// Combination Sum IV
+// leetcode: https://leetcode-cn.com/problems/combination-sum-iv/
+func CombinationSumIV(nums []int, target int) int {
+	dp := make([]int, target+1)
+	dp[0] = 1
+	for i := 1; i <= target; i++ {
+		for _, num := range nums {
+			if i - num >= 0 {
+				dp[i] += dp[i-num]
+			}
+		}
+	}
+	return dp[target]
+}
---
+++ golang/2021-12/combinationSumIV_test.go
@@ -0,0 +1,11 @@
+package _021_12
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCombinationSumIV(t *testing.T) {
+	ans := CombinationSumIV([]int{1,2,3}, 4)
+	fmt.Println(ans)
+}

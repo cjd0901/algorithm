e4fbcaf54b5395258a6917a5b610a34c7af2f231
xdd <80866702@qq.com>
2022-06-21 15:28:14 +0800

    :sparkles: Pow(x, n)

---
+++ golang/2022-06/powxN.go
@@ -0,0 +1,21 @@
+package _022_06
+
+// Pow(x, n)
+// leetcode: https://leetcode.cn/problems/powx-n/
+func Pow(x float64, n int) float64 {
+	if n >= 0 {
+		return quickMul(x, n)
+	}
+	return 1.0 / quickMul(x, -n)
+}
+
+func quickMul(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	y := quickMul(x, n/2)
+	if n%2 == 0 {
+		return y * y
+	}
+	return y * y * x
+}
---
+++ golang/2022-06/powxN_test.go
@@ -0,0 +1,8 @@
+package _022_06
+
+import "testing"
+
+func TestPow(t *testing.T) {
+	ans := Pow(2.0, 6)
+	t.Log(ans)
+}

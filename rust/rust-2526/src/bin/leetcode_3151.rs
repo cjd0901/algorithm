80aa104e6fe62c86ecb7f5af1570f8f34450486a
jd chen <jd.chenx@gmail.com>
2025-12-19 15:51:00 +0800

    leetcode_3151

---
+++ rust/rust-2526/src/bin/leetcode_3151.rs
@@ -0,0 +1,16 @@
+struct Solution {}
+
+impl Solution {
+    pub fn is_array_special(nums: Vec<i32>) -> bool {
+        for i in 1..nums.len() {
+            if nums[i] % 2 == nums[i-1] % 2 {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+fn main() {
+
+}
